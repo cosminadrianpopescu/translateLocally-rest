2f0cea150df8d61ddceee214da7b64d0d11415ee
Cosmin Popescu <cosminadrianpopescu@gmail.com>
2023-09-04 17:55:58 +0200

    set 50mb limit for payloads

--- index.ts
+++ index.ts
@@ -8,7 +8,7 @@ import { TL_PATH } from "./models";
 Injector.provide({provide: TL_PATH, useValue: process.env['TL_PATH']});
 
 const app = express();
-app.use(bodyParser.json());
+app.use(bodyParser.json({limit: '50mb'}));
 const dino = new Dino(app, '/api');
 dino.useRouter(express.Router);
 dino.registerController(TranslatorController);
